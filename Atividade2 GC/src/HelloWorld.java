97d7733cdd4ae33b3170652ba47d77f7b0049301
keiza analizi <keiza.analizi@gmail.com>
2021-07-19 23:56:20 -0300

    Commit master

--- Atividade2 GC/src/HelloWorld.java
+++ Atividade2 GC/src/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld!!!");
+		System.out.println("Keiza");
+		System.out.println("Analizi");
+		
+
 	}
 
 }
